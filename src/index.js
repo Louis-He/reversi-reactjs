2cf42aa111b6000827f3ee73b50a6f13c14081b3
Louis-He <1726110778@qq.com>
2020-12-30 23:23:12 -0500

    alpha version of the platform

--- src/index.js
+++ src/index.js
@@ -3,22 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Switch from '@bit/mui-org.material-ui.switch';
 import FormControlLabel from '@bit/mui-org.material-ui.form-control-label';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from "react-bootstrap";
 
 
 function Square(props) {
     const chess = props.value === 1 ? '●': (props.value === 2 ? '○' : '');
 
     if (props.checkedHint && props.value === 3) {
-        console.log(props)
-
         return (
-            <button style={{color: "orange"}} className="square" onClick={props.onClick}>
+            <button style={{color: "orange", fontFamily: "sans-serif"}} className="square" onClick={props.onClick}>
                 {props.is_x_next ? '●': '○'}
             </button>
         );
     } else {
         return (
-            <button className="square" onClick={props.onClick}>
+            <button style={{fontFamily: "sans-serif"}} className="square" onClick={props.onClick}>
                 {chess}
             </button>
         );
@@ -59,10 +61,15 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
         let original_board = Array(64).fill(0);
+        // initialize the board
         original_board[3 * 8 + 3] = 1;
         original_board[3 * 8 + 4] = 2;
         original_board[4 * 8 + 3] = 2;
         original_board[4 * 8 + 4] = 1;
+
+        let hint_board = original_board.slice();
+
+        this.hintMove(hint_board, true);
 
         this.state = {
             history: [{
@@ -71,7 +78,15 @@ class Game extends React.Component {
             is_x_next: true,
             stepNumber: 0,
             checkedHint: false,
-            latest_squares: original_board,
+            latest_squares: hint_board,
+            is_player_vs_computer: true,
+
+            // dropdown menu
+            dropdown_arr: ["APS105-smarter", "APS105-smartest", "sandbox"],
+            selector: 0,
+
+            // switch side button
+            player: true, // true: black, false: white
         };
     }
 
@@ -93,6 +108,27 @@ class Game extends React.Component {
     }
 
     //############ Helper Functions #############//
+    // check whether the game finishes and if finished, who wins
+    calculateScore(board) {
+        console.log("CS")
+        console.log(board)
+        let black_score = 0;
+        let white_score = 0;
+        for(let i = 0; i < 64; i++){
+            if (board[i] === 1) { black_score++; }
+            else if (board[i] === 2) { white_score++; }
+        }
+
+
+        if((!this.anyMove(board, true)) && (!this.anyMove(board, false))){
+            if ( black_score > white_score ) { return {end: true, win: 1, black: black_score, white: white_score} }
+            else if ( black_score === white_score) { return {end: true, win: 0, black: black_score, white: white_score} }
+            else { return {end: true, win: 2, black: black_score, white: white_score} }
+        }
+
+        return {end: false, black: black_score, white: white_score}
+    }
+
     // Check whether the player has any valid move
     anyMove(board, is_x_next) {
         for(let i = 0; i < 64; i++){
@@ -108,6 +144,8 @@ class Game extends React.Component {
     // return: true if the player has any valid move
     // Note: board will be changed in-place
     hintMove(board, is_x_next) {
+        console.log('hint')
+        console.log(board)
         let any_valid = false;
         for(let i = 0; i < 64; i++){
             const validation_check = this.checkMove(board, i, is_x_next);
@@ -123,7 +161,7 @@ class Game extends React.Component {
     // Return: {move_check: bool, direction_checks: 2D array of bool [3 * 3], msg: string};
     // Notice: If move_check is false, direction_checks will be null
     checkMove(board, i, is_x_next) {
-        if (board[i]) {
+        if (board[i] !== 0 && board[i] !== 3) {
             return {move_check: false, msg: "Spot already taken"};
         }
 
@@ -139,6 +177,7 @@ class Game extends React.Component {
             if(is_legal) { break; }
             let validation_row = []
             for(let look_right = -1; look_right <= 1; look_right++) {
+                if(look_down === 0 && look_right === 0) { validation_row.push(false);continue; }
                 if(is_legal) { break; }
                 let current_xy = JSON.parse(JSON.stringify(target_xy));
                 while(true){
@@ -183,6 +222,8 @@ class Game extends React.Component {
 
         for(let look_right = -1; look_right <= 1; look_right++){
             for(let look_down = -1; look_down <= 1; look_down++){
+                if (look_right === 0 && look_down === 0) {continue;}
+
                 if(validation_check_arr[look_down + 1][look_right + 1]){
                     let current_xy = JSON.parse(JSON.stringify(target_xy));
                     current_xy.x += look_right;
@@ -197,47 +238,185 @@ class Game extends React.Component {
         }
     }
 
+    endMoveWrapper(board, is_x_next) {
+        console.log("endMoveWrapper")
+        console.log(board)
+
+        this.endMove(board, is_x_next);
+        // console.log(next_is_x)
+
+        // if (!(next_is_x === this.state.player)) {
+        //     if (this.state.dropdown_arr[this.state.selector] !== "sandbox"){
+        //         this.opponentMoveWrapper(board);
+        //     }
+        // }
+    }
+
+    endMove(board, is_x_next) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        let is_end = this.calculateScore(board).end;
+        console.log("is_end: " + is_end)
+
+        let hint_board = board.slice();
+
+        this.hintMove(hint_board, !is_x_next);
+
+        let next_player = is_x_next;
+        if (!is_end) {
+            next_player = !is_x_next;
+            if (this.anyMove(board, next_player)) {
+                this.setState({
+                    is_x_next: next_player,
+                })
+            }
+        }
+
+        return this.setState({
+            history: history.concat([{
+                squares: board
+            }]),
+            latest_squares: hint_board,
+            stepNumber: history.length,
+        }, function() {
+            console.log("endMove - callback")
+            if(is_end){
+                return
+            }
+
+            if (!(next_player === this.state.player)) {
+                if (this.state.dropdown_arr[this.state.selector] !== "sandbox"){
+                    this.opponentMove(board);
+                    // console.log(board);
+                }
+            }
+        })
+
+        // return next_player;
+    }
+
+    initGame(player_color) {
+        var that = this;
+        let original_board = Array(64).fill(0);
+        // initialize the board
+        original_board[3 * 8 + 3] = 1;
+        original_board[3 * 8 + 4] = 2;
+        original_board[4 * 8 + 3] = 2;
+        original_board[4 * 8 + 4] = 1;
+
+        this.setState ({
+            is_player_vs_computer: true,
+        });
+
+        if (!player_color && this.state.dropdown_arr[this.state.selector] !== "sandbox") {
+            // request opponent to make a move first
+            this.opponentMove(original_board).then(function(res) {
+                let hint_board = res.slice();
+
+                that.setState({
+                    history: [{
+                        squares: res,
+                    }],
+                    latest_squares: hint_board,
+                    is_x_next: false,
+                    stepNumber: 1,
+                })
+
+                that.hintMove(hint_board, false);
+            })
+        } else {
+            let hint_board = original_board.slice();
+
+            this.setState ({
+                history: [{
+                    squares: original_board,
+                }],
+                is_x_next: true,
+                stepNumber: 0,
+                latest_squares: hint_board,
+            });
+
+            this.hintMove(hint_board, true)
+        }
+    }
+
+    //########## Function used to process AI moves ###########//
+
+    opponentMove(squares) {
+        const formData = new FormData();
+        let boardStr = "";
+
+        for(let i = 0; i < squares.length; i++){
+            boardStr += squares[i];
+        }
+
+        formData.append('board', boardStr);
+        formData.append('color', !this.state.player);
+        formData.append('AI_player', this.state.dropdown_arr[this.state.selector]);
+
+        return fetch('http://localhost:8000/api/get_next', {
+            method: 'POST',
+            body: formData,
+        }).then((response) => response.json()).then((responseData) => {
+            let board_str = responseData.updated_board
+            let board = Array(64).fill(0);
+            for(let i = 0; i < board_str.length; i++){
+                board[i] = parseInt(board_str[i]);
+            }
+
+            this.endMoveWrapper(board, !this.state.player);
+
+            return board;
+        })
+    }
+
     //############ Main click handling Functions #############//
+    changeOpponent(e) {
+        this.setState({
+            selector: e
+        },() => {
+            this.initGame();
+        })
+    }
+
+    changeSide() {
+        var that = this;
+        console.log(this.state.player)
+        this.setState({
+            player: !this.state.player
+        },() => {
+            that.initGame(this.state.player)
+        })
+    }
+
     handleClick(i) {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const squares = history[history.length - 1].squares.slice();
         console.log(history);
 
-        if (calculateWinner(squares) || squares[i]) {
+        if (squares[i] !== 0 && squares[i] !== 3) {
             this.setState({
                 msg: "Invalid move"
             })
             return;
+        } else {
+            this.setState({
+                msg: ""
+            })
         }
 
-        const validation_check = this.checkMove(squares, i, this.state.is_x_next);
+        const validation_check = this.checkMove(squares, i, this.state.is_x_next)
+        console.log(validation_check)
 
         if (validation_check.move_check) {
             this.makeMove(squares, i, validation_check.direction_checks, this.state.is_x_next);
-            let hint_board = squares.slice();
-            const valid_next = this.hintMove(hint_board, !this.state.is_x_next);
-
-            console.log(hint_board)
-
-            this.setState({
-                history: history.concat([{
-                    squares: squares
-                }]),
-                latest_squares: hint_board,
-                stepNumber: history.length,
-                msg: "",
-            });
-            if (valid_next){
-                this.setState({
-                    is_x_next: !this.state.is_x_next,
-                });
-            }
-            console.log(this.state.history)
+            this.endMoveWrapper(squares, this.state.is_x_next)
         } else {
             this.setState({
                 msg: validation_check.msg
             });
         }
+
+        console.log("end handle click")
     }
 
     jumpTo (step) {
@@ -252,13 +431,12 @@ class Game extends React.Component {
     }
 
     handleButton(name, event) {
-        console.log(event);
         this.setState({[name]: event.target.checked});
     }
 
     render() {
         const history = this.state.history;
-        const winner = calculateWinner(this.state.latest_squares);
+        const score = this.calculateScore(this.state.latest_squares);
 
         const moves = history.map((step, move) => {
             const desc = move ?
@@ -273,10 +451,20 @@ class Game extends React.Component {
 
 
         let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
+        let err_msg = this.state.msg;
+        if (score.end) {
+            status = 'Winner: ' + (score.win === 1 ? '⚫' : '⚪');
+            err_msg = "Game already over"
         } else {
-            status = 'Next player: ' + (this.state.is_x_next ? '●' : '○');
+            status = 'Next player: ' + (this.state.is_x_next ? '⚫' : '⚪');
+        }
+
+        let opponent = this.state.dropdown_arr[this.state.selector];
+        let side_info;
+        if (this.state.player) {
+            side_info = "⚫ Player(" + score.black + ") VS. " + opponent + "(" + score.white + ") ⚪";
+        } else {
+            side_info = "⚫ " + opponent + "(" + score.black + ") VS. Player(" + score.white + ") ⚪";
         }
 
         return (
@@ -291,7 +479,22 @@ class Game extends React.Component {
                     />
                 </div>
                 <div className="game-info">
-                    <FormControlLabel
+                    <h5>Choose your opponent</h5>
+
+                    <Dropdown>
+                        <DropdownButton id="dropdown-item-button" title={opponent} onSelect={(e) => this.changeOpponent(e)}>
+                            <Dropdown.Item as="button" eventKey='0'>APS105-smarter</Dropdown.Item>
+                            <Dropdown.Item as="button" eventKey='1'>APS105-smartest</Dropdown.Item>
+                            <Dropdown.Item as="button" eventKey='2'>sandbox</Dropdown.Item>
+                        </DropdownButton>
+                    </Dropdown>
+
+                    <div style={{marginTop: '20px',}}>{side_info}</div>
+                    <div>
+                        <Button style={{marginTop: '10px',}} variant="outline-primary" size="sm" onClick={(e) => this.changeSide()}>Switch Side</Button>
+                    </div>
+
+                    <FormControlLabel style={{marginTop: '10px',}}
                         control={
                             <Switch checked={this.state.checkedHint}
                                     onChange={(e) => {this.handleButton('checkedHint', e)}}
@@ -302,36 +505,33 @@ class Game extends React.Component {
                     />
                     <div>{status}</div>
                     <div style={{color: 'red',}}>{this.state.msg}</div>
-                    <ol>{moves}</ol>
+
+                    {/*<ol>{moves}</ol>*/}
                 </div>
             </div>
         );
     }
 }
 
+class Index extends React.Component {
+
+    render() {
+        return (
+        <div className="Index">
+            <div className="Title" style={{height:"150px", display: "flex", justifyContent: "center", alignItems: "center", fontSize: "48px"}}>
+                APS105 Reversi Interactive Platform
+            </div>
+            <div className="Gameboard" style={{margin: "20px"}}>
+                <Game />
+            </div>
+        </div>
+    )}
+
+}
+
 // ========================================
 
 ReactDOM.render(
-    <Game />,
+    <Index />,
     document.getElementById('root')
 );
-
-function calculateWinner(squares) {
-    // const lines = [
-    //     [0, 1, 2],
-    //     [3, 4, 5],
-    //     [6, 7, 8],
-    //     [0, 3, 6],
-    //     [1, 4, 7],
-    //     [2, 5, 8],
-    //     [0, 4, 8],
-    //     [2, 4, 6],
-    // ];
-    // for (let i = 0; i < lines.length; i++) {
-    //     const [a, b, c] = lines[i];
-    //     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-    //         return squares[a];
-    //     }
-    // }
-    return null;
-}
